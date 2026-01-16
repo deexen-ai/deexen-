97691fac8709a112591244c69faf656f20f62d17
Mr. Algorithm <11aravipratapsingh@gmail.com>
2026-01-17 00:54:05 +0530

    feat: Add signup page with auth integration and proper routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage';
+import SignupPage from '@/pages/SignupPage';
 import ProfilePage from '@/pages/ProfilePage';
 import { useAuthStore } from '@/stores/useAuthStore';
 
@@ -27,6 +28,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route
           path="/profile"
           element={
